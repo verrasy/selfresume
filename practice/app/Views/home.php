ee66300c9812bd797ddc33dfc388e662df4f20a4
verrasy <verrasri16@gmail.com>
2023-05-15 18:32:26 +0700

    final result

--- practice/app/Views/home.php
+++ practice/app/Views/home.php
@@ -28,17 +28,17 @@
     <header class="d-print-none">
       <div class="container text-center text-lg-left">
         <div class="py-3 clearfix">
-          <h1 class="site-title mb-0">Joyce Harrison</h1>
+          <h1 class="site-title mb-0">Verra Sri Yulia Rahmawati</h1>
           <div class="site-nav">
             <nav role="navigation">
               <ul class="nav justify-content-center">
-                <li class="nav-item"><a class="nav-link" href="https://twitter.com/templateflip" title="Twitter"><i class="fab fa-twitter"></i><span class="menu-title sr-only">Twitter</span></a>
+                <li class="nav-item"><a class="nav-link" href="https://twitter.com/raveopuffs" title="Twitter"><i class="fab fa-twitter"></i><span class="menu-title sr-only">Twitter</span></a>
                 </li>
-                <li class="nav-item"><a class="nav-link" href="https://www.facebook.com/templateflip" title="Facebook"><i class="fab fa-facebook"></i><span class="menu-title sr-only">Facebook</span></a>
+                <li class="nav-item"><a class="nav-link" href="https://www.facebook.com/verrasy" title="Facebook"><i class="fab fa-facebook"></i><span class="menu-title sr-only">Facebook</span></a>
                 </li>
-                <li class="nav-item"><a class="nav-link" href="https://www.instagram.com/templateflip" title="Instagram"><i class="fab fa-instagram"></i><span class="menu-title sr-only">Instagram</span></a>
+                <li class="nav-item"><a class="nav-link" href="https://www.instagram.com/verrasy" title="Instagram"><i class="fab fa-instagram"></i><span class="menu-title sr-only">Instagram</span></a>
                 </li>
-                <li class="nav-item"><a class="nav-link" href="https://github.com/templateflip" title="Github"><i class="fab fa-github"></i><span class="menu-title sr-only">Github</span></a>
+                <li class="nav-item"><a class="nav-link" href="https://github.com/verrasy" title="Github"><i class="fab fa-github"></i><span class="menu-title sr-only">Github</span></a>
                 </li>
               </ul>
             </nav>
@@ -52,11 +52,11 @@
   <div class="cover-bg p-3 p-lg-4 text-white">
     <div class="row">
       <div class="col-lg-4 col-md-5">
-        <div class="avatar hover-effect bg-white shadow-sm p-1"><img src="../assets/images/avatar.jpg" width="200" height="200"/></div>
+        <div class="avatar hover-effect bg-white shadow-sm p-1"><img src="../assets/images/sayeu.jpg" width="200" height="200"/></div>
       </div>
       <div class="col-lg-8 col-md-7 text-center text-md-start">
-        <h2 class="h1 mt-2" data-aos="fade-left" data-aos-delay="0">Joyce Harrison</h2>
-        <p data-aos="fade-left" data-aos-delay="100">Graphic Designer & Web Developer</p>
+        <h2 class="h1 mt-2" data-aos="fade-left" data-aos-delay="0">Verra Sri Yulia Rahmawati</h2>
+        <p data-aos="fade-left" data-aos-delay="100">Data Analyst, UI/UX Designer, Frontend Developer, and Copywriter</p>
         <div class="d-print-none" data-aos="fade-left" data-aos-delay="200"><a class="btn btn-light text-dark shadow-sm mt-1 me-1" href="right-resume.pdf" target="_blank">Download CV</a><a class="btn btn-success shadow-sm mt-1" href="#contact">Hire Me</a></div>
       </div>
     </div>
@@ -65,7 +65,7 @@
     <div class="row">
       <div class="col-md-6">
         <h2 class="h3 mb-3">About Me</h2>
-        <p>Hello! I’m Joyce Harrison. I am passionate about UI/UX design and Web Design. I am a skilled Front-end Developer and master of Graphic Design tools such as Photoshop and Sketch.</p>
+        <p>Hello! I'm Verra. Third-year outstanding student of Informatics Engineering, enthusiastic about advanced things with a positive impact value, with 2 years of involvement in organizations, and consistently participating and learning about technological developments. Certified in Front-End Website Development using React JS and Mobile Apps Development using Kotlin, skillful at UI/UX using Figma, and immense interest in Data Science or Analyst.</p>
       </div>
       <div class="col-md-5 offset-md-1">
         <div class="row mt-2">
@@ -73,25 +73,25 @@
             <div class="pb-1">Age</div>
           </div>
           <div class="col-sm-8">
-            <div class="pb-1 text-secondary">28</div>
+            <div class="pb-1 text-secondary">21</div>
           </div>
           <div class="col-sm-4">
             <div class="pb-1">Email</div>
           </div>
           <div class="col-sm-8">
-            <div class="pb-1 text-secondary">Joyce@company.com</div>
+            <div class="pb-1 text-secondary">verrasri16@gmail.com</div>
           </div>
           <div class="col-sm-4">
             <div class="pb-1">Phone</div>
           </div>
           <div class="col-sm-8">
-            <div class="pb-1 text-secondary">+0718-111-0011</div>
+            <div class="pb-1 text-secondary">+62-877-2177-8247</div>
           </div>
           <div class="col-sm-4">
             <div class="pb-1">Address</div>
           </div>
           <div class="col-sm-8">
-            <div class="pb-1 text-secondary">140, City Center, New York, U.S.A</div>
+            <div class="pb-1 text-secondary">Sukabumi, Jawa Barat, Indonesia</div>
           </div>
         </div>
       </div>
@@ -119,17 +119,17 @@
         </div>
       </div>
       <div class="col-md-6">
-        <div class="mb-2"><span>Adobe Photoshop</span>
+        <div class="mb-2"><span>Figma</span>
           <div class="progress my-1">
             <div class="progress-bar bg-success" role="progressbar" data-aos="zoom-in-right" data-aos-delay="400" data-aos-anchor=".skills-section" style="width: 80%" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
           </div>
         </div>
-        <div class="mb-2"><span>Sketch</span>
+        <div class="mb-2"><span>Python</span>
           <div class="progress my-1">
             <div class="progress-bar bg-success" role="progressbar" data-aos="zoom-in-right" data-aos-delay="500" data-aos-anchor=".skills-section" style="width: 85%" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100"></div>
           </div>
         </div>
-        <div class="mb-2"><span>Adobe XD</span>
+        <div class="mb-2"><span>Microsoft Office</span>
           <div class="progress my-1">
             <div class="progress-bar bg-success" role="progressbar" data-aos="zoom-in-right" data-aos-delay="600" data-aos-anchor=".skills-section" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
           </div>
@@ -143,23 +143,23 @@
     <div class="timeline">
       <div class="timeline-card timeline-card-primary card shadow-sm">
         <div class="card-body">
-          <div class="h5 mb-1">Frontend Developer <span class="text-muted h6">at Creative Agency</span></div>
-          <div class="text-muted text-small mb-2">May, 2015 - Present</div>
-          <div>Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition.</div>
+          <div class="h5 mb-1">Frontend Developer <span class="text-muted h6">at Alterra Academy</span></div>
+          <div class="text-muted text-small mb-2">April, 2023 - Present</div>
+          <div>Develop admin web for focus group discussion called Discuss.In</div>
         </div>
       </div>
       <div class="timeline-card timeline-card-primary card shadow-sm">
         <div class="card-body">
-          <div class="h5 mb-1">Graphic Designer <span class="text-muted h6">at Design Studio</span></div>
-          <div class="text-muted text-small mb-2">June, 2013 - May, 2015</div>
-          <div>Override the digital divide with additional clickthroughs from DevOps. Nanotechnology immersion along the information highway will close the loop on focusing solely on the bottom line.</div>
+          <div class="h5 mb-1">UI/UX Designer <span class="text-muted h6">at GoodThings Studio</span></div>
+          <div class="text-muted text-small mb-2">November, 2021 - present</div>
+          <div>Co Foundering company, create banner design for hijab store.</div>
         </div>
       </div>
       <div class="timeline-card timeline-card-primary card shadow-sm">
         <div class="card-body">
-          <div class="h5 mb-1">Junior Web Developer <span class="text-muted h6">at Indie Studio</span></div>
-          <div class="text-muted text-small mb-2">Jan, 2011 - May, 2013</div>
-          <div>User generated content in real-time will have multiple touchpoints for offshoring. Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment.</div>
+          <div class="h5 mb-1">Graphic Designer <span class="text-muted h6">at Pitlab Indonesia</span></div>
+          <div class="text-muted text-small mb-2">Mei 2022</div>
+          <div>Redesign the company proposal using Canva.</div>
         </div>
       </div>
     </div>
@@ -171,33 +171,33 @@
     <div class="timeline">
       <div class="timeline-card timeline-card-success card shadow-sm">
         <div class="card-body">
-          <div class="h5 mb-1">Masters in Information Technology <span class="text-muted h6">from International University</span></div>
-          <div class="text-muted text-small mb-2">2011 - 2013</div>
-          <div>Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition.</div>
+          <div class="h5 mb-1">Bachelor in Information Technology <span class="text-muted h6">from Universitas Muhammadiyah Sukabumi</span></div>
+          <div class="text-muted text-small mb-2">2020 - 2024</div>
+          <div>Third-year outstanding student of Informatics Engineering, enthusiastic about advanced things with a positive impact value, with 2 years of involvement in organizations, and consistently participating and learning about technological developments.</div>
         </div>
       </div>
       <div class="timeline-card timeline-card-success card shadow-sm">
         <div class="card-body">
-          <div class="h5 mb-1">Bachelor of Computer Science <span class="text-muted h6">from Regional College</span></div>
+          <div class="h5 mb-1">Science <span class="text-muted h6">from SMA Negeri 3 Sukabumi</span></div>
           <div class="text-muted text-small mb-2">2007 - 2011</div>
-          <div>Override the digital divide with additional clickthroughs from DevOps. Nanotechnology immersion along the information highway will close the loop on focusing solely on the bottom line.</div>
+          <div>2 years involvement at Karya Ilmiah Remaja</div>
         </div>
       </div>
-      <div class="timeline-card timeline-card-success card shadow-sm">
+      <!-- <div class="timeline-card timeline-card-success card shadow-sm">
         <div class="card-body">
           <div class="h5 mb-1">Science and Mathematics <span class="text-muted h6">from Mt. High Scool</span></div>
           <div class="text-muted text-small mb-2">1995 - 2007</div>
           <div>User generated content in real-time will have multiple touchpoints for offshoring. Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment.</div>
         </div>
       </div>
-    </div>
+    </div> -->
   </div>
   <hr class="d-print-none"/>
   <div class="contant-section px-3 px-lg-4 pb-4" id="contact">
     <h2 class="h3 text mb-3">Contact</h2>
     <div class="row">
       <div class="col-md-7 d-print-none">
-        <div class="my-2"><form action="https://formspree.io/your@email.com"
+        <div class="my-2"><form action="verrasri16@gmail.com"
     method="POST">
   <div class="row">
     <div class="col-6">
@@ -217,11 +217,11 @@
       <div class="col">
         <div class="mt-2">
           <h3 class="h6">Address</h3>
-          <div class="pb-2 text-secondary">140, City Center, New York, U.S.A</div>
+          <div class="pb-2 text-secondary">Sukabumi, Jawa Barat, Indonesia</div>
           <h3 class="h6">Phone</h3>
-          <div class="pb-2 text-secondary">+0718-111-0011</div>
+          <div class="pb-2 text-secondary">+62-877-2177-8247</div>
           <h3 class="h6">Email</h3>
-          <div class="pb-2 text-secondary">Joyce@company.com</div>
+          <div class="pb-2 text-secondary">verrasri16@gmail.com</div>
         </div>
       </div>
       <div class="col d-none d-print-block">
@@ -253,7 +253,7 @@
     <footer class="pt-4 pb-4 text-muted text-center d-print-none">
       <div class="container">
         <div class="my-3">
-          <div class="h4">Joyce Harrison</div>
+          <div class="h4">Verra Sri Yulia Rahmawati</div>
           <div class="footer-nav">
             <nav role="navigation">
               <ul class="nav justify-content-center">
@@ -270,8 +270,7 @@
           </div>
         </div>
         <div class="text-small">
-          <div class="mb-1">&copy; Right Resume. All rights reserved.</div>
-          <div>Design - <a href="https://templateflip.com/" target="_blank">TemplateFlip</a></div>
+          <div class="mb-1">&copy; Verra's Resume. All rights reserved.</div>
         </div>
       </div>
     </footer>
